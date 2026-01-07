8545c26d7eb92c631d40244c54afeaf88b63b14f
vijay <vijaykumarsimar1>
2026-01-07 15:02:29 +0530

    base url

--- src/Base-url.jsx
+++ src/Base-url.jsx
@@ -1,7 +1,9 @@
 
 
 export const baseUrlFun = () => {
-    const BASE_URL = "http://localhost:8597";
+    // const BASE_URL = "http://localhost:8597";
+    const BASE_URL = "https://onecard-backend-myog.onrender.com";
+
     return BASE_URL;
 };
 
